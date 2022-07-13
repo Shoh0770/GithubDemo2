e8738c28c0a48b7d5b6463890f0b8b407fbb7f1f
Ziloliddin <Zilolidddinkamolov@gmail.com>
2022-07-13 21:26:14 +0500

    Initial Commit

---
+++ GithubDemo2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubDemo2
+//
+//  Created by Macbook Pro 14 Inch on 7/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubDemo2/GithubDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemo2App.swift
+//  GithubDemo2
+//
+//  Created by Macbook Pro 14 Inch on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
